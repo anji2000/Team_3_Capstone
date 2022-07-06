aa912dd9a7607dee9cf804720878e4592a8155ec
Anjaneya <guntaanjaneya@gmail.com>
2022-07-06 10:03:53 -0700

    second commit

--- features/functions.feature
+++ features/functions.feature
@@ -5,7 +5,14 @@
     Given open sample app
     And  enter user name and password
     And  click on login
-    Then check response
+    Then check response 
+    And  enter user name and password try one
+    And  click on login one
+    Then check response of one
+    And  enter user name and password try two
+    And  click on login two
+    Then check response two
+   
     
 
  
--- src/com/team3/StepDef/functions_StepDef.java
+++ src/com/team3/StepDef/functions_StepDef.java
@@ -55,6 +55,54 @@ public class functions_StepDef {
      	String actualtitle= driver.findElement(By.xpath("/html/body/section/div/div[1]/div/label")).getText();
      	Assert.assertEquals(" sample app is not avilable", expectedtitle, actualtitle);
 	}
+	
+	@And("^enter user name and password try one$")
+	public void fill_details_one() throws Throwable 
+	{
+		driver.findElement(By.name("UserName")).sendKeys("xyz",Keys.ENTER);
+	   	driver.findElement(By.name("Password")).sendKeys("XYZ",Keys.ENTER);	
+	}
+	
+	@And("^click on login one$")
+	public void login_one() throws Throwable 
+	{
+		driver.findElement(By.id("login")).click();
+
+	}
+
+
+	@Then("^check response of one$")
+	public void validate_user_one() throws Throwable 
+	{
+		String expectedtitle = "Invalid username/password";
+	   	String actualtitle= driver.findElement(By.xpath("/html/body/section/div/div[1]/div/label")).getText();
+	   	Assert.assertEquals(" sample app is not avilable", expectedtitle, actualtitle);
+	}
+
+	@And("^enter user name and password try two$")
+	public void fill_details_two() throws Throwable 
+	{
+		driver.findElement(By.name("UserName")).sendKeys("xyz",Keys.ENTER);
+	   	driver.findElement(By.name("Password")).sendKeys("pwd",Keys.ENTER);
+	}
+	
+	@And("^click on login two$")
+	public void login_two() throws Throwable 
+	{
+		driver.findElement(By.id("login")).click();	
+	   	driver.findElement(By.id("login")).click();
+
+
+	}
+
+
+	@Then("^check response two$")
+	public void validate_user_two() throws Throwable 
+	{
+		String expectedtitle = "User logged out.";
+	   	String actualtitle= driver.findElement(By.xpath("/html/body/section/div/div[1]/div/label")).getText();
+	   	Assert.assertEquals(" sample app is not avilable", expectedtitle, actualtitle);
+	}
 
 
 }
